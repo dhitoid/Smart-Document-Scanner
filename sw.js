b5c0fd55ebf710c60322e61ab16352c0fb9c0474
DT DEV ID <153781142+dhitoid@users.noreply.github.com>
2026-06-16 00:44:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,25 @@
+const CACHE_NAME = 'billsplitter-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',
+  'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'
+];
+
+// Tahap Install: Simpan file ke cache offline
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open('scanner-cache').then((cache) => {
-        return cache.addAll(['/', '/index.html']);
-    }));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Tahap Fetch: Ambil data dari cache jika sedang tidak ada internet
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
